fdb4c0673064df9cb6955e953060ae4b8faf84ce
Andrey Chernoprudov <achernoprudov@naumen.ru>
2017-03-06 15:28:21 +0500

    Initial Commit

---
+++ PagedViewContainer/PagedViewContainer.h
@@ -0,0 +1,19 @@
+//
+//  PagedViewContainer.h
+//  PagedViewContainer
+//
+//  Created by Андрей Чернопрудов on 06/03/2017.
+//  Copyright © 2017 Little Stars. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PagedViewContainer.
+FOUNDATION_EXPORT double PagedViewContainerVersionNumber;
+
+//! Project version string for PagedViewContainer.
+FOUNDATION_EXPORT const unsigned char PagedViewContainerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PagedViewContainer/PublicHeader.h>
+
+
